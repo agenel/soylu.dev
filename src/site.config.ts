fbea7a537e6f25347a312f18631d9f596b2eb13f
agenel <soyluarif@gmail.com>
2024-08-11 03:38:37 +0200

    maintaince11

--- src/site.config.ts
+++ src/site.config.ts
@@ -5,9 +5,9 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "Arif Soylu",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Arif Soylu | Personal website",
+	title: "Arif Soylu | Professional Overview",
 	// Meta property used as the default description meta property
-	description: "Arif Soylu personal website, portfolio, and blog",
+	description: "Arif Soylu personal website, professional overview, portfolio, and blog",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
